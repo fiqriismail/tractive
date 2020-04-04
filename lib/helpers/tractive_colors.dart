51e4d38a8d57bbe1a85127a634ba6f0ff4d00b13
Fiqri Ismail <fiqri.ismail@gmail.com>
2020-04-04 11:42:48 +0530

    Home screen completed

---
+++ lib/helpers/tractive_colors.dart
@@ -0,0 +1,16 @@
+import 'dart:ui' show Color;
+
+class TractivColors {
+  // Primary Colors
+  static const Color sage = Color(0xFF87BCBF);
+  static const Color rust = Color(0xFFD97D54);
+  static const Color drab = Color(0xFF324755);
+  static const Color oynx = Color(0xFF1B1C20);
+  static const Color slate = Color(0xFF6E8CA0);
+  static const Color snow = Color(0xFFFFFFFF);
+
+  // Secondary Colors
+  static const Color ice = Color(0xFFF0F3F4);
+  static const Color fossil = Color(0xFFC8D1D3);
+  static const Color sand = Color(0xFFB9B0A2);
+}
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,109 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+import '../helpers/tractive_colors.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          color: TractivColors.drab,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/splash.jpg',
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  fit: BoxFit.fill,
+                ),
+                Positioned(
+                  bottom: 40,
+                  child: Image.asset('assets/images/logo_white.png'),
+                )
+              ],
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    'Track Your',
+                    style: TextStyle(
+                        fontFamily: 'Coolvetica',
+                        fontSize: 40,
+                        color: TractivColors.snow),
+                  ),
+                  Text(
+                    'Active Lifestyle',
+                    style: TextStyle(
+                        fontFamily: 'Coolvetica',
+                        fontSize: 40,
+                        color: TractivColors.snow),
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    width: MediaQuery.of(context).size.width * 0.3,
+                    child: Divider(
+                      thickness: 2,
+                      color: TractivColors.snow,
+                    ),
+                  ),
+                  Text(
+                    'With goal a driven approach',
+                    style: TextStyle(
+                        fontFamily: 'Coolvetica',
+                        fontSize: 25,
+                        color: TractivColors.snow),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.1,
+                color: TractivColors.rust,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text(
+                            'Get Started',
+                            style: TextStyle(
+                                fontFamily: 'BigNoodle',
+                                color: TractivColors.snow,
+                                letterSpacing: 3,
+                                fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                        height: double.infinity,
+                        child: FlatButton(
+                          color: Color(0xFFC4714B),
+                          child: Icon(
+                            Icons.navigate_next,
+                            color: TractivColors.snow,
+                          ),
+                          onPressed: () {},
+                        ))
+                  ],
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
